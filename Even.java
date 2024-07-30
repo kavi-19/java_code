a9eb34e153995502723fe7b1edbf833ba93f0477
kavi-19 <kavithakaari1924@gmail.com>
2024-07-30 17:32:06 +0530

    initial commit

---
+++ Even.java
@@ -0,0 +1,10 @@
+public class Even {
+  public static void main(String[] args){
+    for(int i=0;i<=50;i++){
+      if(i%2==0){
+        System.out.println("The number is even:"+" "+i);
+      }
+    }
+  }
+  
+}
